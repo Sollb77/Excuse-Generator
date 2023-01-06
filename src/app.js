127153c82f2151b09b9c59f9f2c83618ab7541c0
Sollb77 <qsiecuadorservicios@gmail.com>
2023-01-06 15:17:50 +0000

    trabajo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload =() => {
+  document.querySelector('#btn').addEventListener('click', () =>{
+      document.querySelector('#the-excuse').innerHTML = generateExcuse();
+  });
+  
 };
+
+let generateExcuse = () => {
+let pronoun = ['A','The'];
+let subject = ['clown','dolphin','dog','baby','driver','comedian'];
+let action= ['took my','threw my', 'yelled at my','stole my','bit my'];
+let possetion= ['homework','toe', 'car','shoe'];
+let where= ['on the street','in my house', 'in my driveway'];
+
+let proIndex= Math.floor(Math.random()*pronoun.length);
+let subIndex= Math.floor(Math.random()*subject.length);
+let actIndex= Math.floor(Math.random()*action.length);
+let possIndex= Math.floor(Math.random()*possetion.length);
+let whereIndex= Math.floor(Math.random()*where.length);
+
+return pronoun[proIndex]+' '+subject[subIndex]+' '+action[actIndex]+' '+possetion[possIndex]+' '+where[whereIndex]
+
+
+}
